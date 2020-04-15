0a7b83dae538e71be353d7c0f3a57bc32225434d
Ingrid Carlson <ingridclaire@gmail.com>
2020-04-14 17:51:25 -0700

    we implemented readAll, readOne and are working on update!

--- datastore/index.js
+++ datastore/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+const Promise = require('bluebird');
+const readFilePromise = Promise.promisify(fs.readFile);
 
 var items = {};
 
@@ -18,6 +20,7 @@ exports.create = (text, callback) => {
         if (err) {
           throw err;
         }
+        items[id] = text;
         //we commented out the line below, and our todos were still added to data file. What is this callback doing?
         callback(null, { id, text });
 
@@ -27,41 +30,71 @@ exports.create = (text, callback) => {
 };
 
 exports.readAll = (callback) => {
-  let path = exports.dataDir;
-  fs.readdir(path, (err, items) => {
+
+  let filePath = exports.dataDir;
+
+  fs.readdir(filePath, (err, files) => {
+
     if (err) {
-      console.error('error when defining readAll and invoking readdir', err);
-    } else {
-
-      var data = _.map(items, (text, id) => {
-        // format this
-        return { id, text };
-      });
-
-      callback(null, data);
+      return callback(err);
     }
 
+    var data = _.map(files, (file) => {
+      var id = path.basename(file, '.txt');
+      var ourFilePath = path.join(filePath, file);
+      return readFilePromise(ourFilePath).then((fileData) => {
+        return {
+          id: id,
+          // text: id
+          text: fileData.toString()
+        };
+      });
+    });
+
+    Promise.all(data).then((items) => {
+      callback(null, items);
+    });
+
+  });
+};
+
+exports.readOne = (id, callback) => {
+
+  let filePath = exports.dataDir;
+  let ourFilePath = path.join(filePath, `${id}.txt`);
+
+  fs.readFile(ourFilePath, (err, data) => {
+    if (err) {
+      return callback(err);
+    } else {
+      callback(null, { id, text: data.toString() });
+    }
   });
 
 };
 
-exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
-};
-
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+
+  let filePath = exports.dataDir;
+  let ourFilePath = path.join(filePath, `${id}.txt`);
+
+  fs.writeFile(ourFilePath, text, (err) => {
+    var item = items[id];
+    if (!item) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      items[id] = text;
+      callback(null, { id: id, text: text });
+    }
+
+  });
+
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.delete = (id, callback) => {
